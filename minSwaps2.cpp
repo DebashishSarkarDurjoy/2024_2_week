b119212e5252fbb2ee4fe23256fed4d10396cba7
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2024-02-06 22:53:01 +1100

    min swaps template

---
+++ minSwaps2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int minSwaps(vector<int> nums) {
+
+    return 0;
+}
+
+int main(void) {
+    vector<int> numbers = {2,4,5,1,3};
+    cout << minSwaps(numbers) << endl;
+
+    return 0;
+}
