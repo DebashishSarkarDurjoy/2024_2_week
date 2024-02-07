15c24be43cdcf7e43ac37fea0c19b0d8bb193334
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2024-02-08 02:27:01 +1100

    min swaps p2

--- minSwaps2.cpp
+++ minSwaps2.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 int minSwaps(vector<int> nums) {
+    int n = nums.size();
 
-    return 0;
+    pair<int,int> pa[n];
+    for (int i = 0; i < nums.size(); i++) {
+        pa[i] = {nums[i], i}; 
+    }
+
+    sort(pa, pa+n);
+
+    vector<bool> visited(n, false);
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int cycle = 0;
+        int node = i;
+        while (!visited[node]) {
+            visited[node] = true;
+            cycle++;
+            node = pa[node].second;
+        }
+        if (cycle) ans += (cycle - 1);
+    }
+
+
+
+    return ans;
 }
 
 int main(void) {
